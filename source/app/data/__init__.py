9496c9d437344cb6c318be84e69d6b96a1dae89e
Lukas Kubis <contact@lukaskubis.com>
2017-05-18 19:44:41 +0200

    removed print statement

--- source/app/data/__init__.py
+++ source/app/data/__init__.py
@@ -6,7 +6,6 @@ from .dbsession import *
 
 def init_db(db_folder):
     rel_folder = os.path.join('db', 'muggledb.sqlite')
-    print(os.path.join(db_folder, rel_folder))
     DBSessionFactory.global_init(os.path.join(db_folder, rel_folder))
 
 class Wrapper():
