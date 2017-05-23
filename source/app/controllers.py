0b7c3584145ed3f0a4b5a8033a60ee3e994d2cf8
Lukas Kubis <contact@lukaskubis.com>
2017-05-23 14:16:28 +0200

    static cache

--- source/app/controllers.py
+++ source/app/controllers.py
@@ -1,11 +1,32 @@
 # controllers.py
 
+import hashlib
+from os import path
 from pyramid_handlers import action
 
 
 class Controller:
     def __init__(self, request):
         self.request = request
+
+    def cache(self, static):
+        if not static:
+            return 'ERROR_NO_FILE:\n ({})'.format(static)
+
+        # resolve paths to static content
+        fullpath = path.dirname(path.abspath(__file__))
+        fullname = path.abspath(path.join(fullpath, static.lstrip('/')))
+
+        if not path.exists(fullname):
+            return 'ERROR_MISSING_FILE:\n ({})'.format(fullname)
+
+        # get file hash
+        md5 = hashlib.md5()
+        with open(fullname, 'rb') as fin:
+            data = fin.read()
+            md5.update(data)
+
+        return static + '?cacheId=' + md5.hexdigest()
 
 
 class HomeController(Controller):
